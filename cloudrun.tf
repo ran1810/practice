18d0da7411e8e3103f4d6cf5418bc6563c67cbd8
ran1810 <101968070+ran1810@users.noreply.github.com>
2024-07-03 20:57:04 +0530

    Update cloudrun.tf

--- cloudrun.tf
+++ cloudrun.tf
@@ -16,3 +16,19 @@ resource "google_cloud_run_service" "cloudrun" {
   }
 
 }
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "policy" {
+  location = google_cloud_run_service.cloudrun.location
+  project = google_cloud_run_service.cloudrun.project
+  service = google_cloud_run_service.cloudrun.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
